9693841a50a912f358e7dc77081b10b70cb57ce4
Davide Lasagna <davide.lasagna@soton.ac.uk>
2016-05-15 21:49:45 +0100

    Added docs and improved code comments

--- src/ArraySlices.jl
+++ src/ArraySlices.jl
@@ -8,20 +8,28 @@ export slices, columns, rows
 # 
 # F : a SubArray
 # N : ndims of the slices
-# D : indexed dimension of the parent
-# P : parent array type
+# D : indexed dimension of the array
+# A : array type
 # 
-immutable SliceIterator{F, N, D, P<:AbstractArray} <: AbstractVector{F}
-    parent::P
+immutable SliceIterator{F, N, D, A<:AbstractArray} <: AbstractVector{F}
+    array::A
 end
 
-@generated function slices{T, N, D}(parent::AbstractArray{T, N}, ::Type{Val{D}})
+"""
+    slices(array, dim)
+
+Return a `SliceIterator` object to loop over the slices of `array` along
+dimension `dim`.
+
+"""
+@generated function slices{T, N, D}(array::AbstractArray{T, N}, ::Type{Val{D}})
+    # checks
     1 <= D <= N || error("invalid slice dimension")
     
-    # ~~~ Construct type of slice - incomplete, then fill
-    F = :(SubArray{$T, $N-1, typeof(parent)})
+    # construct incomplete type of slice, then fill
+    F = :(SubArray{$T, $N-1, typeof(array)})
     
-    # construct tuple of indexes 
+    # construct tuple of indices
     tupexpr = :(Tuple{})
     for i = 1:N 
         push!(tupexpr.args, :Colon)
@@ -33,30 +41,52 @@ end
     D == 1 ? push!(F.args, N) : push!(F.args, D)
 
     # build and return iterator
-    :(SliceIterator{$F, $N-1, $D, typeof(parent)}(parent))
+    :(SliceIterator{$F, $N-1, $D, typeof(array)}(array))
 end
-# allow creating slices without the Val{d} business
-slices(parent::AbstractArray, d::Integer) = slices(parent, Val{d})
 
-# Array interface
+# allow creating slices without the Val{d} business
+slices(array::AbstractArray, d::Integer) = slices(array, Val{d})
+
+
+
+# ~~~ Array interface ~~~
 eltype{F}(s::SliceIterator{F}) = F
-length{F, N, D}(s::SliceIterator{F, N, D}) = size(s.parent, D)
-size{F, N, D}(s::SliceIterator{F, N, D}) = (size(s.parent, D), )
+length{F, N, D}(s::SliceIterator{F, N, D}) = size(s.array, D)
+size{F, N, D}(s::SliceIterator{F, N, D}) = (size(s.array, D), )
+
+# build code that produces slices with the correct indexing
 @generated function getindex{F, N, D}(s::SliceIterator{F, N, D}, i::Integer)
-    # Build code that produces slices with the correct indexing
     expr = :()
     expr.head = :call                        
     push!(expr.args, :slice)
-    push!(expr.args, :(getfield(s, :parent)))
-    for i = 1:(N+1) # fill in with `Colon`s
+    push!(expr.args, :(getfield(s, :array)))
+    # fill in with `Colon`s
+    for i = 1:(N+1) 
         push!(expr.args, Colon())
     end
-    expr.args[2 + D] = :i # replace the indexed dimension
+    # then replace the indexed dimension
+    expr.args[2 + D] = :i 
     expr
 end
 
-# convenience functions for 2D arrays
-columns(parent::AbstractMatrix) = slices(parent, Val{2})
-rows(parent::AbstractMatrix) = slices(parent, Val{1})
+
+
+# ~~~ Convenience functions for 2D arrays ~~~
+
+"""
+    columns(array)
+
+Return a `SliceIterator` object to loop over the columns of `array`.
+
+"""
+columns(array::AbstractMatrix) = slices(array, Val{2})
+
+"""
+    rows(array)
+
+Return a `SliceIterator` object to loop over the rows of `array`.
+
+"""
+rows(array::AbstractMatrix) = slices(array, Val{1})
 
 end
